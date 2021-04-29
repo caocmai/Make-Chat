d093af803b2f1476f2adb7afd27c818ad59882a6
Cao Mai <caomai@rocketmail.com>
2021-04-28 21:05:21 -0700

    Messages go to correct channel

--- sockets/chat.js
+++ sockets/chat.js
@@ -16,38 +16,41 @@ module.exports = (io, socket, onlineUsers, channels) => {
   });
 
   //Listen for new messages
-  socket.on('new message', (data) => {
+  socket.on("new message", (data) => {
     // Send that data back to ALL clients
-    console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
-    io.emit('new message', data);
-  })
+    console.log(`🎤 ${data.sender}: ${data.message} 🎤`);
 
-  socket.on('get online users', () => {
+    //Save the new message to the channel.
+    channels[data.channel].push({ sender: data.sender, message: data.message });
+
+    //Emit only to sockets that are in that channel room.
+    io.to(data.channel).emit("new message", data);
+  });
+
+  socket.on("get online users", () => {
     //Send over the onlineUsers
-    socket.emit('get online users', onlineUsers);
-  })
+    socket.emit("get online users", onlineUsers);
+  });
 
   // This fires when a user closes out of the application
-// socket.on("disconnect") is a special listener that fires when a user exits out of the application.
-socket.on('disconnect', () => {
-  //This deletes the user by using the username we saved to the socket
-  delete onlineUsers[socket.username]
-  io.emit('user has left', onlineUsers);
-});
-
-socket.on('new channel', (newChannel) => {
-  //Save the new channel to our channels object. The array will hold the messages.
-  channels[newChannel] = [];
-  //Have the socket join the new channel room.
-  socket.join(newChannel);
-  //Inform all clients of the new channel.
-  io.emit('new channel', newChannel);
-  //Emit to the client that made the new channel, to change their channel to the one they made.
-  socket.emit('user changed channel', {
-    channel : newChannel,
-    messages : channels[newChannel]
+  // socket.on("disconnect") is a special listener that fires when a user exits out of the application.
+  socket.on("disconnect", () => {
+    //This deletes the user by using the username we saved to the socket
+    delete onlineUsers[socket.username];
+    io.emit("user has left", onlineUsers);
   });
-});
 
-  
+  socket.on("new channel", (newChannel) => {
+    //Save the new channel to our channels object. The array will hold the messages.
+    channels[newChannel] = [];
+    //Have the socket join the new channel room.
+    socket.join(newChannel);
+    //Inform all clients of the new channel.
+    io.emit("new channel", newChannel);
+    //Emit to the client that made the new channel, to change their channel to the one they made.
+    socket.emit("user changed channel", {
+      channel: newChannel,
+      messages: channels[newChannel],
+    });
+  });
 };
