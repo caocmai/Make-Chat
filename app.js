8e8ed8a1232ea7c0e2fdc3d4aa2b02e171948df3
Cao Mai <caomai@rocketmail.com>
2021-04-28 20:42:48 -0700

    Users can join and leave chats

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 //app.js
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
-
+const server = require("http").Server(app);
 
 //Socket.io
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
+
+//We'll store our online users here
+let onlineUsers = {};
 io.on("connection", (socket) => {
   console.log("🔌 New user connected! 🔌");
-  require('./sockets/chat.js')(io, socket);
-
-})
-
+  // Make sure to send the users to our chat file
+  require("./sockets/chat.js")(io, socket, onlineUsers);
+});
 
 //Express View Engine for Handlebars
-const exphbs  = require('express-handlebars');
-app.engine('handlebars', exphbs({ defaultLayout: "" })); // to fix handlebar not found set defaultLayout: ""
-app.set('view engine', 'handlebars');
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "" })); // to fix handlebar not found set defaultLayout: ""
+app.set("view engine", "handlebars");
 
 //Establish your public folder
-app.use('/public', express.static('public'))
+app.use("/public", express.static("public"));
 
+app.get("/", (req, res) => {
+  console.log("jaskldfjlaksdjlfk");
+  res.render("index.handlebars");
+});
 
-app.get('/', (req, res) => {
-   console.log("jaskldfjlaksdjlfk")
-  res.render('index.handlebars');
-})
-
-server.listen('3000', () => {
-  console.log('Server listening on Port 3000');
-})
+server.listen("3000", () => {
+  console.log("Server listening on Port 3000");
+});
